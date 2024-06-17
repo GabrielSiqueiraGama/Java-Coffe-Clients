cd971dba62e164cd98f0ea18b17ba5bb411e1ab4
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-06-17 12:25:11 -0300

    feat: :seedling: showing images from Products

--- src/pages/produtos/Produto.tsx
+++ src/pages/produtos/Produto.tsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+interface Produto {
+  id: number;
+  nome: string;
+  preco: number;
+  descricao: string;
+  imagem: string;
+}
+
 const Produto = () => {
+  const [data, setData] = useState<Produto[]>([]);
 
-  const [data, setData] = useState([]);
-
-  useEffect(() =>{
-    axios.get('http://localhost:8080/api/cardapio').then(res => setData(res.data)).catch(err => console.log(err))
-  }, [])
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/cardapio')
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <div className='mt-3'>
@@ -16,25 +25,27 @@ const Produto = () => {
         <thead>
           <tr>
             <th>Id:</th>
-            <th>Nome: </th>
-            <th>Preço: </th>
-            <th>Descrição: </th>
-            <th>Imagem: </th>
+            <th>Nome:</th>
+            <th>Preço:</th>
+            <th>Descrição:</th>
+            <th>Imagem:</th>
           </tr>
-          <tbody>
-            {
-              data.map((produto, index) =>{
-                return <tr key={index}>
-                  <td>{produto.id}</td>
-                  <td>{produto.nome}</td>
-                  <td>{produto.preco}</td>
-                  <td>{produto.descricao}</td>
-                  <td>{produto.imagem}</td>
-                </tr>
-              })
-            }
-          </tbody>
         </thead>
+        <tbody>
+          {
+            data.map((produto, index) => (
+              <tr key={index}>
+                <td>{produto.id}</td>
+                <td>{produto.nome}</td>
+                <td>{produto.preco}</td>
+                <td>{produto.descricao}</td>
+                <td>
+                  <img src={produto.imagem} alt={produto.nome} style={{ width: '100px', height: '100px' }} />
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     </div>
   );
