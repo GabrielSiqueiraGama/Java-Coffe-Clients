e876a409a23d09e7482f34585cedce9c137db49b
GabrielSiqueiraGama <Siqueiragamgabriel@gmail.com>
2024-06-18 14:33:31 -0300

    style: :art: customizing the footer

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import styles from '../assets/css/Footer.module.css';
+import { Link, NavLink } from 'react-router-dom'
 const logo = require('../assets/images/logoJava.png');
 
 const Footer = () =>{
   return(
-    <div className={styles.body}>
-      <img src={logo}/>
-      <p>Java Coffe</p>
-  </div>
+    <footer className={styles.footer}>
+      <div>
+        <NavLink to='/Home' className={styles.links}>
+          <div className={styles.logo}>
+            <img src={logo}/>
+            <p>Java Coffe</p>
+          </div>
+        </NavLink>
+        <div  className={styles.midias}>
+          <p>w</p>
+          <p>f</p>
+          <p>i</p>
+        </div>
+      </div>
+      
+      <div>
+        <p><NavLink to='/Cardapio' className={styles.links}>Cardapio</NavLink></p>
+      </div>
+      <div>
+        <p><NavLink to='/Franqueado' className={styles.links}> Franqueado</NavLink></p>
+      </div>
+      <div>
+        <p><NavLink to='/' className={styles.links}> Franqueado</NavLink></p>
+      </div>
+    </footer>
   )
 }
 export default Footer
